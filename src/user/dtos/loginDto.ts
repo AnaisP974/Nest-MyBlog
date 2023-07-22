f9860b93b21d304a35f9b17fc90793477a17255c
Anaïs PERIGNY <anaisperigny31@gmail.com>
2023-07-22 16:00:05 +0200

    Mise en place de la connexion

--- src/user/dtos/loginDto.ts
+++ src/user/dtos/loginDto.ts
@@ -1,16 +1,5 @@
 import { IsString, IsEmail, Length, IsNotEmpty, MinLength, MaxLength } from "class-validator";
-export class SignupDto {
-
-  @IsString()
-  @IsNotEmpty()
-  @Length(3, 50)
-  @MinLength(3, {
-    message: "Doit contenir un minimum de 3 caractères."
-  })
-  @MaxLength(50, {
-    message: "Doit contenir un maximum de 50 caractères."
-  })
-  readonly username: string;
+export class LoginDto {
   
   @IsEmail()
   readonly email: string;
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,26 +1,35 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { SignupDto } from './dtos/signupDto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { Repository } from 'typeorm';
 import * as bcrypt from "bcrypt";
+import { LoginDto } from './dtos/loginDto';
 
 @Injectable()
 export class UserService {
-  constructor(
-    @InjectRepository(User)
-    private readonly usersRepository: Repository<User>,
-  ) {}
+  constructor(@InjectRepository(User)private readonly usersRepository: Repository<User>,) {}
   
   async postSignup(body: SignupDto): Promise<string> {
-    try{
+    try {
         const {password} = body
         const hash = await bcrypt.hash(password, 10)
         const user = this.usersRepository.create({...body, password : hash})
         await this.usersRepository.save(user)
         return "User created !"
-    } catch (error) {
+    } 
+    catch (error) {
         throw new ConflictException(error.message)
     }
   }
+
+  async postLogin(body: LoginDto) {
+    const {password, email} = body ;
+    const user = await this.usersRepository.findOne({where: {email : email}});
+    if(!user) throw new NotFoundException("User Not Found")
+    const match = await bcrypt.compare(password, user.password)
+    if(!match) throw new UnauthorizedException("Invalid password")
+    return user
+  }
 }
+
